5ee1ef5934814c62996ee213f7972c95c63916f5
Gavin Tronset <gtronset@gmail.com>
2020-01-17 19:17:13 -0800

    Extracts background functions into own module
    Moves background image-related functions into their own module, privatizing functions as needed.

--- src/js/background_lists/default-backgrounds.js
+++ src/js/background_lists/default-backgrounds.js
@@ -4,7 +4,7 @@ function _importAll(r) {
     return r.keys().map(r);
 }
 
-const images = _importAll(require.context('../images/default_backgrounds/', false, /\.(png|jpe?g|svg)$/));
+const images = _importAll(require.context('../../images/default_backgrounds/', false, /\.(png|jpe?g|svg)$/));
 
 const default_backgrounds = images.map(function(img){
     return { 'url': img.default };
