72c20240f52a238686e415ad4f0aca71755a9dff
Gavin Tronset <gtronset@gmail.com>
2020-01-18 12:53:33 -0800

    Extracts weather functions into Weather module & rename WeatherClient
    Moves "weather" functions into new Weather module, privatizing functions as needed, and renames existing Weather module to WeatherClient.

--- src/js/app.js
+++ src/js/app.js
@@ -1,54 +1,20 @@
-import Variables from './variables.js';
-import Icons from './icons.js';
-import Weather from './weather.js';
+import Variables from './variables';
+import Weather from './weather';
 import Backgrounds from './backgrounds';
 import Time from './time';
 
-import Utilities from './utilities';
-const { toTitleCase } = Utilities;
+const intialize = () => {
+    const BackgroundsList = Backgrounds.getList(Variables.backgrounds);
 
-const BackgroundsList = Backgrounds.getList(Variables.backgrounds);
-
-/* Weather */
-
-function updateWeatherInformation(current){
-    const weatherSelector = document.getElementById('weather');
-    const iconSelector = weatherSelector.querySelector('.icon');
-
-    const temperature = Math.floor(current.temperature());
-    const weatherCond = toTitleCase(current.conditions());
-
-    const icon = Icons.getIcon(current.icon());
-
-    const iconRegex = /(^|\s)i-\S+/g;
-
-    weatherSelector.querySelector('.temp').innerHTML = temperature;
-
-    if(iconSelector.className.match(iconRegex)){
-        iconSelector.className = iconSelector.className.replace(iconRegex, '');
-    }
-    iconSelector.classList.add(icon.iconClass);
-
-    Icons.changeFavicon(`images/icons/${icon.iconName}.ico`);
-
-    weatherSelector.querySelector('.description').innerHTML = weatherCond;
-
-    document.querySelector('title').innerHTML = `${temperature}° ${weatherCond}`;
-}
-
-function displayWeather(){
-    Weather.getCurrent(Variables.default_city, updateWeatherInformation);
-
-    setTimeout(displayWeather, Variables.cycle_duration);
-}
-
-/* Initialize */
-
-document.addEventListener('DOMContentLoaded', function(){
     Backgrounds.initialize(BackgroundsList, Variables.cycle_duration);
 
     Time.initialize();
 
-    Weather.apiKey = Variables.weather_api_key;
-    displayWeather();
-});
+    Weather.initialize(
+        Variables.weather_api_key,
+        Variables.default_city,
+        Variables.cycle_duration
+    );
+};
+
+document.addEventListener('DOMContentLoaded', intialize);
--- src/js/weather.js
+++ src/js/weather.js
@@ -1,166 +1,48 @@
-var Weather = {
-    Utils: {},
-    apiKey: ''
+import Icons from './icons';
+import WeatherClient from './weather-client';
+
+import Utilities from './utilities';
+const { delay, toTitleCase } = Utilities;
+
+function updateWeatherInformation(current){
+    const weatherSelector = document.getElementById('weather');
+    const iconSelector = weatherSelector.querySelector('.icon');
+
+    const temperature = Math.floor(current.temperature());
+    const weatherCond = toTitleCase(current.conditions());
+
+    const icon = Icons.getIcon(current.icon());
+
+    const iconRegex = /(^|\s)i-\S+/g;
+
+    weatherSelector.querySelector('.temp').innerHTML = temperature;
+
+    if(iconSelector.className.match(iconRegex)){
+        iconSelector.className = iconSelector.className.replace(iconRegex, '');
+    }
+    iconSelector.classList.add(icon.iconClass);
+
+    Icons.changeFavicon(`images/icons/${icon.iconName}.ico`);
+
+    weatherSelector.querySelector('.description').innerHTML = weatherCond;
+
+    document.querySelector('title').innerHTML = `${temperature}° ${weatherCond}`;
+}
+
+function displayWeather(city, cycle_duration = 30000){
+    delay(cycle_duration).then(() => {
+        WeatherClient.getCurrent(city, updateWeatherInformation);
+
+        displayWeather(city, cycle_duration);
+    });
+}
+
+const weather = {
+    initialize: function(weather_api_key, city, cycle_duration){
+        WeatherClient.apiKey = weather_api_key;
+
+        displayWeather(city, cycle_duration);
+    }
 };
 
-Weather.VERSION = '0.0.4';
-
-var jsonp = Weather.Utils.jsonp = function (uri){
-    return new Promise(function(resolve, reject){
-        var id = '_' + Math.round(10000 * Math.random());
-        var callbackName = 'jsonp_callback_' + id;
-        var el = (document.getElementsByTagName('head')[0] || document.body || document.documentElement);
-        var script = document.createElement('script');
-        var src = uri + '&callback=' + callbackName;
-
-        window[callbackName] = function(data){
-            delete window[callbackName];
-            var ele = document.getElementById(id);
-            ele.parentNode.removeChild(ele);
-            resolve(data);
-        };
-
-        script.src = src;
-        script.id = id;
-        script.addEventListener('error', reject);
-        el.appendChild(script);
-    } );
-};
-
-Weather.kelvinToFahrenheit = function (value) {
-    return (this.kelvinToCelsius(value) * 1.8) + 32;
-};
-
-Weather.kelvinToCelsius = function (value) {
-    return value - 273.15;
-};
-
-Weather.getCurrent = function (city, callback) {
-    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + encodeURIComponent(city) + '&units=imperial&&APPID=' + Weather.apiKey;
-
-    return this._getJSON(url, function (data) {
-        callback(new Weather.Current(data));
-    } );
-};
-
-Weather.getForecast = function (city, callback) {
-    var url = 'http://openweathermap.org/data/2.1/forecast/city?q=' + encodeURIComponent(city) + '&cnt=1';
-
-    return this._getJSON(url, function (data) {
-        callback(new Weather.Forecast(data));
-    } );
-};
-
-Weather._getJSON = function(url, callback) {
-    jsonp(url).then(callback);
-};
-
-var maxBy = Weather.Utils.maxBy = function (list, iterator) {
-    var max;
-    var f = function (memo, d) {
-        var val = iterator(d);
-
-        if (memo === null || val > max) {
-            max = val;
-            memo = d;
-        }
-
-        return memo;
-    };
-
-    return list.reduce(f, null);
-};
-
-var minBy = Weather.Utils.minBy = function (list, iterator) {
-    var min;
-    var f = function (memo, d) {
-        var val = iterator(d);
-
-        if (memo === null || val < min) {
-            min = val;
-            memo = d;
-        }
-
-        return memo;
-    };
-
-    return list.reduce(f, null);
-};
-
-var isOnDate = Weather.Utils.isOnDate = function (a, b) {
-    var sameYear = a.getYear() === b.getYear();
-    var sameMonth = a.getMonth() === b.getMonth();
-    var sameDate = a.getDate() === b.getDate();
-
-    return sameYear && sameMonth && sameDate;
-};
-
-Weather.Forecast = function (data) {
-    this.data = data;
-};
-
-Weather.Forecast.prototype.startAt = function () {
-    return new Date(minBy(this.data.list, function (d) { return d.dt; }).dt * 1000);
-};
-
-Weather.Forecast.prototype.endAt = function () {
-    return new Date(maxBy(this.data.list, function (d) { return d.dt; }).dt * 1000);
-};
-
-Weather.Forecast.prototype.day = function (date) {
-    return new Weather.Forecast(this._filter(date));
-};
-
-Weather.Forecast.prototype.low = function () {
-    if (this.data.list.length === 0) return;
-
-    var output = minBy(this.data.list, function (item) {
-        return item.main.temp_min;
-    } );
-
-    return output.main.temp_min;
-};
-
-Weather.Forecast.prototype.high = function () {
-    if (this.data.list.length === 0) return;
-
-    var output = maxBy( this.data.list, function (item) {
-        return item.main.temp_max;
-    } );
-
-    return output.main.temp_max;
-};
-
-Weather.Forecast.prototype._filter = function (date) {
-    return {
-        list: this.data.list.filter(function (range) {
-            var dateTimestamp = (range.dt * 1000);
-
-            if (isOnDate(new Date(dateTimestamp), date)) {
-                return range;
-            }
-        })
-    };
-};
-
-Weather.Current = function (data) {
-    //console.log(data)
-    this.data = data;
-};
-
-Weather.Current.prototype.temperature = function () {
-    return this.data.main.temp;
-};
-
-Weather.Current.prototype.conditions = function () {
-    return this.data.weather[0].description;
-};
-
-Weather.Current.prototype.icon = function () {
-    return {
-        code: this.data.weather[0].icon,
-        id: this.data.weather[0].id
-    };
-};
-
-export default Weather;
+export default weather;
