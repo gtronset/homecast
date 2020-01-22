3a38aae7c5102987509a38b9da71ae822289111a
Gavin Tronset <gtronset@gmail.com>
2020-01-21 18:50:30 -0800

    Add basic lint-staged for testing

--- tests/app.test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import App from '../src/js/app';
-
-test('_getBackgrounds returns background, "default" by default', () => {
-    const _getBackgrounds = App.__get__('_getBackgrounds');
-
-    expect(Array.isArray(_getBackgrounds('custom-backgrounds'))).toBe(true);
-});
