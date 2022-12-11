e6b8e0332b03b47c7cfba93cecbf20c5ee079dff
Gavin Tronset <gtronset@gmail.com>
2022-12-11 14:03:51 -0800

    Fix fonts paths

--- webpack.config.js
+++ webpack.config.js
@@ -70,15 +70,11 @@ module.exports = {
                 ],
             },
             {
-                test: /\.(woff|woff2|ttf|otf|eot)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'fonts',
-                        },
-                    },
-                ],
+                test: /\.(woff|woff2|ttf|otf|eot)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[hash][ext][query]',
+                },
             },
         ],
     },
